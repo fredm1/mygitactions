a8348fdae039ace731053345d6a75f1ba086e34e
fredm1 <alfredo_martinez_1@yahoo.com>
2023-05-07 14:37:49 -0700

    Update main.py
    testing

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def hello():
-    print("hi")
+import fire
 
-
-def bye():
-    print("bye")
-
-
-print(hello())
+def hello(name="World"):
+  return "Hello %s! Welcome to my world! " % name
+def test(name):
+  if name:
+    return True
+  else:
+    return False
+  retrun True
+  
+if __name__ == '__main__':
+  fire.Fire(hello)
